3a68b5edfafb5825eaddc03f61859076ff91ae3a
PeterfriisJensen <55391201+PeterfriisJensen@users.noreply.github.com>
2020-10-22 13:11:14 +0200

    Class Category

--- ADOExample/Program.cs
+++ ADOExample/Program.cs
@@ -9,6 +9,9 @@ namespace ADOExample
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
+
         public override string ToString()
         {
             return $"Id = {Id}, Name = {Name}";
