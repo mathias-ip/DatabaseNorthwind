66baa4f106bbdd9292f3dfe80d9e1932d73d5353
Mathias <mip@ruc.dk>
2020-10-22 14:15:53 +0200

    ddd

--- ADOExample/DataService.cs
+++ ADOExample/DataService.cs
@@ -3,8 +3,23 @@ namespace ADOExample
 {
     public class DataService
     {
-        public DataService()
+        public void GetCategories()
         {
+
+
+
+            var dataService = new DataService();
+
+
+            using var ctx = new NorthWindContext();
+
+            foreach (var product in ctx.Products.Include(x => x.Category))
+            {
+                Console.WriteLine(product);
+            }
+
+
+
         }
     }
 }
