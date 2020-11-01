8e4639419532f2d7ae3679d73d2be8fd30363958
PeterfriisJensen <55391201+PeterfriisJensen@users.noreply.github.com>
2020-11-01 15:17:20 +0100

    Local Host

--- ADOExample/NorthWindContext.cs
+++ ADOExample/NorthWindContext.cs
@@ -21,7 +21,7 @@ namespace ADOExample
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseLoggerFactory(MyLoggerFactory);
-            optionsBuilder.UseNpgsql("host=localhost;db=Northwind;uid=postgres;pwd=wqm97rcn8844");
+            optionsBuilder.UseNpgsql("host=localhost;db=Northwind;uid=postgres;pwd=Wqm97rcn");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
